ba08e9b4c36fd5b7b116e289780a16ba0e941904
Saurav017 <sauravabde@gmail.com>
2022-12-24 14:42:48 +0530

    code updation

--- client/script.js
+++ client/script.js
@@ -97,7 +97,7 @@ const handleSubmit = async (event) => {
 
   // fetch data from the server
 
-  const response = await fetch('http://localhost:5000/', {
+  const response = await fetch('https://chatdot.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
